2d47bbc653cf50a15ea6eb5ef7d75fda34966955
ronny1982 <wegener.ronny@gmail.com>
2021-11-14 12:36:48 +0100

    Updated app/package icons

--- Anime SD2HD/MainWindow.xaml.cs
+++ Anime SD2HD/MainWindow.xaml.cs
@@ -1,16 +1,31 @@
 ﻿using AnimeSD2HD.Properties;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Runtime.InteropServices;
 using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace AnimeSD2HD
 {
     internal sealed partial class MainWindow : Window
     {
+        const int IMAGE_ICON = 1;
+        const int LR_LOADFROMFILE = 0x10;
+        const int WM_SETICON = 0x80;
+        const int ICON_SMALL = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr LoadImage(IntPtr hInst, string lpsz, uint uType, int cxDesired, int cyDesired, uint fuLoad);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam);
+
         private ConfigurationViewModel Configuration { get; }
 
         public MainWindow(ConfigurationViewModel configuration)
         {
+            LoadIcon(@"Properties\App.ico");
             Title = Resources.WindowTitle;
             Configuration = configuration;
             Configuration.Dispatcher = DispatcherQueue;
@@ -18,10 +33,17 @@ namespace AnimeSD2HD
             InitializeComponent();
         }
 
+        private void LoadIcon(string identifier)
+        {
+            var hwnd = WindowNative.GetWindowHandle(this);
+            var icon = LoadImage(IntPtr.Zero, identifier, IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
+            SendMessage(hwnd, WM_SETICON, ICON_SMALL, icon);
+        }
+
         private FileOpenPicker CreateFilePicker(params string[] extensions)
         {
             var picker = new FileOpenPicker();
-            WinRT.Interop.InitializeWithWindow.Initialize(picker, WinRT.Interop.WindowNative.GetWindowHandle(this));
+            InitializeWithWindow.Initialize(picker, WindowNative.GetWindowHandle(this));
             foreach(var extension in extensions)
             {
                 picker.FileTypeFilter.Add(extension);
